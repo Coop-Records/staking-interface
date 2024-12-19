3699448071a279099ffb6fa609da6161181c2de5
Sweets Sweetman <sweetmantech@gmail.com>
2024-12-19 10:10:08 -0500

    remove redundant react import.

--- providers/Providers.tsx
+++ providers/Providers.tsx
@@ -1,8 +1,7 @@
-import { type ReactNode } from "react";
 import PrivyProvider from "./PrivyProvider";
 import QueryProvider from "./QueryProvider";
 
-export default function Providers({ children }: { children: ReactNode }) {
+export default function Providers({ children }: { children: React.ReactNode }) {
   return (
     <QueryProvider>
       <PrivyProvider>{children}</PrivyProvider>
