a9340f028038a434e81e9fe0594885125d2fbdc1
Sweets Sweetman <sweetmantech@gmail.com>
2024-12-19 11:11:53 -0500

    New UnlockDate component.

--- components/Balances.tsx
+++ components/Balances.tsx
@@ -1,8 +1,8 @@
 "use client";
 
 import { useBalances } from "@/hooks/useBalances";
-import { formatUnlockDate } from "@/lib/utils/formatDate";
 import { formatEther } from "viem";
+import { UnlockDate } from "./UnlockDate";
 
 export function Balances() {
   const { data: balances, isLoading } = useBalances();
@@ -19,26 +19,15 @@ export function Balances() {
   const stakedBalance = formatEther(balances.stakedBalance ?? BigInt(0));
   const allowance = formatEther(balances.allowance ?? BigInt(0));
 
-  const depositTime = balances.depositTimestamp
-    ? Number(balances.depositTimestamp) * 1000
-    : 0;
-  const lockDurationSeconds = balances.lockDuration
-    ? Number(balances.lockDuration)
-    : 0;
-  const unlockTime = depositTime + lockDurationSeconds * 1000;
-
   return (
     <div className="flex flex-col gap-2 text-sm">
       <div>Available: {parseFloat(tokenBalance).toFixed(0)} IJN</div>
       <div>Staked: {parseFloat(stakedBalance).toFixed(0)} IJN</div>
       <div>Approved for staking: {parseFloat(allowance).toFixed(0)} IJN</div>
-      {depositTime > 0 && (
-        <div>
-          <div className="text-xs text-gray-500">
-            Unlocks on {formatUnlockDate(unlockTime)}
-          </div>
-        </div>
-      )}
+      <UnlockDate
+        depositTimestamp={balances.depositTimestamp}
+        lockDuration={balances.lockDuration}
+      />
     </div>
   );
 }
