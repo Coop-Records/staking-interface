14e2fdc71346fe5e0e412532b2a985d18cd70095
Sweets Sweetman <sweetmantech@gmail.com>
2024-12-19 09:43:22 -0500

    onEnter opens login.

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,19 @@
 "use client";
 
 import LoginButton from "@/components/LoginButton";
+import LogoutButton from "@/components/LogoutButton";
 import { StakeInput } from "@/components/StakeInput";
+import { usePrivy } from "@privy-io/react-auth";
 
 export default function Home() {
+  const { authenticated } = usePrivy();
+
   return (
     <main className="min-h-screen p-8">
       <div className="max-w-md mx-auto mt-20">
         <div className="flex items-center w-full justify-between">
           <h1 className="text-2xl font-bold mb-8">Stake IJN</h1>
-          <LoginButton />
+          {authenticated ? <LogoutButton /> : <LoginButton />}
         </div>
         <StakeInput />
       </div>
--- components/StakeInput.tsx
+++ components/StakeInput.tsx
@@ -1,9 +1,17 @@
 "use client";
 
+import { usePrivy } from "@privy-io/react-auth";
 import { useState } from "react";
 
 export function StakeInput() {
   const [stakeAmount, setStakeAmount] = useState<string>("");
+  const { login } = usePrivy();
+
+  const handleKeyDown = (e: React.KeyboardEvent<HTMLInputElement>) => {
+    if (e.key === "Enter") {
+      login();
+    }
+  };
 
   return (
     <div className="flex flex-col gap-2">
@@ -18,7 +26,8 @@ export function StakeInput() {
         placeholder="Enter amount of IJN"
         value={stakeAmount}
         onChange={(e) => setStakeAmount(e.target.value)}
-        className="w-full px-4 py-2 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-white [appearance:textfield] [&::-webkit-outer-spin-button]:appearance-none [&::-webkit-inner-spin-button]:appearance-none"
+        onKeyDown={handleKeyDown}
+        className="w-full px-4 py-2 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-white text-black [appearance:textfield] [&::-webkit-outer-spin-button]:appearance-none [&::-webkit-inner-spin-button]:appearance-none"
       />
     </div>
   );
